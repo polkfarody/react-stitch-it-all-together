b1c68b2f36482d7cfad16e9b332cf32ea6b12ace
Polk Farody <polkfarody@gmail.com>
2020-04-09 13:19:41 +1000

    Move login form into it's own directory

---
+++ src/components/LoginComponent.js
@@ -0,0 +1,46 @@
+import React, {Component} from 'react';
+import {Button, Form, FormGroup, Input, Label} from "reactstrap";
+
+class Login extends Component {
+  constructor(props) {
+    super(props);
+
+    this.toggleModal = props.toggleModal;
+
+    this.handleLogin = this.handleLogin.bind(this);
+  }
+
+  handleLogin(event) {
+    this.toggleModal();
+
+    alert('Username: ' + this.username.value + 'Password: ' + this.password.value + 'Remember: ' + this.remember.checked);
+
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+        <>
+          <Form onSubmit={this.handleLogin}>
+            <FormGroup>
+              <Label htmlFor="username">Username</Label>
+              <Input type="text" id="username" name="username" innerRef={(input) => this.username = input}/>
+            </FormGroup>
+            <FormGroup>
+              <Label htmlFor="password">Password</Label>
+              <Input type="password" id="password" name="password" innerRef={(input) => this.password = input}/>
+            </FormGroup>
+            <FormGroup check>
+              <Label check>
+                <Input type="checkbox" name="remember" innerRef={(input) => this.remember = input}/>
+                Remember Me
+              </Label>
+            </FormGroup>
+            <Button type="submit" value="value" color="primary">Login</Button>
+          </Form>
+        </>
+    );
+  }
+}
+
+export default Login;
